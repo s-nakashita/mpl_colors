793a50c72b93b23dac26b6cf1f7d1dfddbe42a26
Saori Nakashita <nakashita@dpac.dpri.kyoto-u.ac.jp>
2024-03-26 11:39:12 +0900

    colormaps.py added

---
+++ colormaps.py
@@ -0,0 +1,105 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+cmaps = [('Perceptually Uniform Sequential', [
+            'viridis', 'plasma', 'inferno', 'magma', 'cividis']),
+         ('Sequential', [
+            'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+            'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+            'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']),
+         ('Sequential (2)', [
+            'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink',
+            'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia',
+            'hot', 'afmhot', 'gist_heat', 'copper']),
+         ('Diverging', [
+            'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+            'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']),
+         ('Cyclic', ['twilight', 'twilight_shifted', 'hsv']),
+         ('Qualitative', [
+            'Pastel1', 'Pastel2', 'Paired', 'Accent',
+            'Dark2', 'Set1', 'Set2', 'Set3',
+            'tab10', 'tab20', 'tab20b', 'tab20c']),
+         ('Miscellaneous', [
+            'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern',
+            'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg',
+            'gist_rainbow', 'rainbow', 'jet', 'turbo', 'nipy_spectral',
+            'gist_ncar'])]
+
+gradient = np.linspace(0, 1, 256)
+gradient = np.vstack((gradient, gradient))
+
+
+def plot_color_gradients(cmap_category, cmap_list):
+    # Create figure and adjust figure height to number of colormaps
+    nrows = len(cmap_list)
+    figh = 0.35 + 0.15 + (nrows + (nrows-1)*0.1)*0.22
+    fig, axs = plt.subplots(nrows=nrows, figsize=(6.4, figh))
+    fig.subplots_adjust(top=1-.35/figh, bottom=.15/figh, left=0.2, right=0.99)
+
+    axs[0].set_title(f"{cmap_category} colormaps", fontsize=14)
+
+    for ax, cmap_name in zip(axs, cmap_list):
+        ax.imshow(gradient, aspect='auto', cmap=cmap_name)
+        ax.text(-.01, .5, cmap_name, va='center', ha='right', fontsize=10,
+                transform=ax.transAxes)
+
+    # Turn off *all* ticks & spines, not just the ones with colormaps.
+    for ax in axs:
+        ax.set_axis_off()
+
+    figname=f"{cmap_category}".replace(" ","_").replace("(","").replace(")","")
+    fig.savefig(f"{figname}.png",dpi=300)
+
+if __name__ == "__main__":
+#    for cmap_category, cmap_list in cmaps:
+#        plot_color_gradients(cmap_category, cmap_list)
+    cmap=plt.get_cmap("PiYG")
+    cmaptmp=cmap.resampled(8)
+    print("cmaptmp")
+    print(cmaptmp.N)
+    print(cmaptmp(0.0));print(cmaptmp(0.5));print(cmaptmp(1.0))
+    cl0 = cmap(cmap.N//2)
+    cl1 = cmap(cmap.N//4*3)
+    cl2a = cmap(cmap.N-1)
+    cl2b = cmap(0)
+    cl3 = cmap(cmap.N//4)
+    cl4 = cmap(cmap.N//2-1)
+    cdict = {
+    'red':(
+        (0.0, cl0[0], cl0[0]),
+        (0.25, cl1[0], cl1[0]),
+        (0.5, cl2a[0], cl2b[0]),
+        (0.75, cl3[0], cl3[0]),
+        (1.0, cl4[0], cl4[0]),
+    ),
+    'green':(
+        (0.0, cl0[1], cl0[1]),
+        (0.25, cl1[1], cl1[1]),
+        (0.5, cl2a[1], cl2b[1]),
+        (0.75, cl3[1], cl3[1]),
+        (1.0, cl4[1], cl4[1]),
+    ),
+    'blue':(
+        (0.0, cl0[2], cl0[2]),
+        (0.25, cl1[2], cl1[2]),
+        (0.5, cl2a[2], cl2b[2]),
+        (0.75, cl3[2], cl3[2]),
+        (1.0, cl4[2], cl4[2]),
+    ),
+    'alpha':(
+        (0.0, cl0[3], cl0[3]),
+        (0.25, cl1[3], cl1[3]),
+        (0.5, cl2a[3], cl2b[3]),
+        (0.75, cl3[3], cl3[3]),
+        (1.0, cl4[3], cl4[3]),
+    ),
+    }
+    mycmap = mpl.colors.LinearSegmentedColormap('GrPi',cdict)
+    print("mycmap")
+    print(mycmap.N)
+    print(mycmap(0.0));print(mycmap(0.5));print(mycmap(1.0))
+
+    mpl.colormaps.register(mycmap)
+    plot_color_gradients("mycmap", ['PiYG','GrPi'])
+
